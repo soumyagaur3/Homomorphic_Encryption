189ce8ac0e8cd06a522a5c757df05a1d192cc2aa
Soumy Gaur <155235016+soumyagaur3@users.noreply.github.com>
2025-03-01 17:55:42 +0530

    Add files via upload

---
+++ Project/o.py
@@ -0,0 +1,265 @@
+import numpy as np
+import time
+from numpy.polynomial import polynomial as poly
+
+
+
+#modification
+
+def polymul(x, y, modulus, poly_mod):
+    n = len(x)
+    if n == 1:
+        return x * y % modulus
+    else:
+        X = (x[0] + x[1]) * (y[0] + y[1]) % modulus
+        Y = x[0] * y[0] % modulus
+        Z = x[1] * y[1] % modulus
+        mid = (X - Y - Z) % modulus
+        return np.int64(np.round(poly.polydiv(np.array([Y] + [mid] + [Z]), poly_mod)[1] % modulus))
+
+def polyadd(x, y, modulus, poly_mod):
+    if isinstance(x, int):
+        x = np.array([x])
+    if isinstance(y, int):
+        y = np.array([y])
+    n = max(len(x), len(y))
+    x = np.pad(x, (0, n - len(x)), mode='constant')
+    y = np.pad(y, (0, n - len(y)), mode='constant')
+    return (x + y) % modulus
+
+
+#upto here
+
+def gen_binary_poly(size):
+    """Generates a polynomial with coefficients in [0, 1]
+    Args:
+        size: number of coefficients, size-1 being the degree of the
+            polynomial.
+    Returns:
+        array of coefficients with the coeff[i] being
+        the coeff of x ^ i.
+    """
+    return np.random.randint(0, 2, size, dtype=np.int64)
+
+
+def gen_uniform_poly(size, modulus):
+    """Generates a polynomial with coefficients being integers in Z_modulus
+    Args:
+        size: number of coefficients, size-1 being the degree of the
+            polynomial.
+    Returns:
+        array of coefficients with the coeff[i] being
+        the coeff of x ^ i.
+    """
+    return np.random.randint(0, modulus, size, dtype=np.int64)
+
+
+def gen_normal_poly(size):
+    """Generates a polynomial with coefficients in a normal distribution
+    of mean 0 and astandard deviation of 2, then discretize it.
+    Args:
+        size: number of coefficients, size-1 being the degree of the
+            polynomial.
+    Returns:
+        array of coefficients with the coeff[i] being
+        the coeff of x ^ i.
+    """
+    return np.int64(np.random.normal(0, 2, size=size))
+
+
+def keygen(size, modulus, poly_mod):
+    """Generate a public and secret keys
+    Args:
+        size: size of the polynoms for the public and secret keys.
+        modulus: coefficient modulus.
+        poly_mod: polynomial modulus.
+    Returns:
+        Public and secret key.
+    """
+    sk = gen_binary_poly(size)
+    a = gen_uniform_poly(size, modulus)
+    e = gen_normal_poly(size)
+    b = polyadd(polymul(-a, sk, modulus, poly_mod), -e, modulus, poly_mod)
+    return (b, a), sk
+
+
+def encrypt(pk, size, q, t, poly_mod, pt):
+    """Encrypt an integer.
+    Args:
+        pk: public-key.
+        size: size of polynomials.
+        q: ciphertext modulus.
+        t: plaintext modulus.
+        poly_mod: polynomial modulus.
+        pt: integer to be encrypted.
+    Returns:
+        Tuple representing a ciphertext.
+    """
+    delta = q // t
+    scaled_m = delta * (pt % t)  % q
+    e1 = gen_normal_poly(size)
+    e2 = gen_normal_poly(size)
+    u = gen_binary_poly(size)
+    ct0 = polyadd(
+            polyadd(
+                polymul(pk[0], u, q, poly_mod),
+                e1, q, poly_mod),
+            scaled_m, q, poly_mod
+        )
+    ct1 = polyadd(
+            polymul(pk[1], u, q, poly_mod),
+            e2, q, poly_mod
+        )
+    return (ct0, ct1)
+
+def decrypt(sk, size, q, t, poly_mod, ct):
+    """Decrypt a ciphertext
+    Args:
+        sk: secret-key.
+        size: size of polynomials.
+        q: ciphertext modulus.
+        t: plaintext modulus.
+        poly_mod: polynomial modulus.
+        ct: ciphertext.
+    Returns:
+        Integer representing the plaintext.
+    """
+    scaled_pt = polyadd(
+            polymul(ct[1], sk, q, poly_mod),
+            ct[0], q, poly_mod
+        )
+    decrypted_poly = np.floor(scaled_pt * t / q) % t
+    return int(decrypted_poly[0]) % 256
+
+
+def decrypt(sk, size, q, t, poly_mod, ct):
+    """Decrypt a ciphertext
+    Args:
+        sk: secret-key.
+        size: size of polynomials.
+        q: ciphertext modulus.
+        t: plaintext modulus.
+        poly_mod: polynomial modulus.
+        ct: ciphertext.
+    Returns:
+        Integer representing the plaintext.
+    """
+    scaled_pt = polyadd(
+            polymul(ct[1], sk, q, poly_mod),
+            ct[0], q, poly_mod
+        )
+    decrypted_poly = np.floor(scaled_pt * t / q) % t
+    return int(decrypted_poly[0]) % 256
+
+
+def decrypt(sk, size, q, t, poly_mod, ct):
+    """Decrypt a ciphertext
+    Args:
+        sk: secret-key.
+        size: size of polynomials.
+        q: ciphertext modulus.
+        t: plaintext modulus.
+        poly_mod: polynomial modulus.
+        ct: ciphertext.
+    Returns:
+        Integer representing the plaintext.
+    """
+    scaled_pt = polyadd(
+            polymul(ct[1], sk, q, poly_mod),
+            ct[0], q, poly_mod
+        )
+    decrypted_poly = np.round(scaled_pt * t / q) % t
+    return int(decrypted_poly[0])
+
+
+
+
+
+
+def add_plain(ct, pt, q, t, poly_mod):
+    """Add a ciphertext and a plaintext.
+    Args:
+        ct: ciphertext.
+        pt: integer to add.
+        q: ciphertext modulus.
+        t: plaintext modulus.
+poly_mod: polynomial modulus.
+    Returns:
+        Tuple representing a ciphertext.
+    """
+    size = len(poly_mod) - 1
+    # encode the integer into a plaintext polynomial
+    m = np.array([pt] + [0] * (size - 1), dtype=np.int64) % t
+    delta = q // t
+    scaled_m = delta * m  % q
+    new_ct0 = polyadd(ct[0], scaled_m, q, poly_mod)
+    return (new_ct0, ct[1])
+
+
+def mul_plain(ct, pt, q, t, poly_mod):
+    """Multiply a ciphertext and a plaintext.
+    Args:
+        ct: ciphertext.
+        pt: integer to multiply.
+        q:ciphertext modulus.
+        t: plaintext modulus.
+        poly_mod: polynomial modulus.
+    Returns:
+        Tuple representing a ciphertext.
+    """
+    size = len(poly_mod) - 1
+    # encode the integer into a plaintext polynomial
+    m = np.array([pt] + [0] * (size - 1), dtype=np.int64) % t
+    new_c0 = polymul(ct[0], m, q, poly_mod)
+    new_c1 = polymul(ct[1], m, q, poly_mod)
+    return (new_c0, new_c1)
+
+
+# Scheme's parameters
+# polynomial modulus degree
+n = 12**4
+# ciphertext modulus
+q = 3**20
+# plaintext modulus
+t = 2**16
+# polynomial modulus
+poly_mod = np.array([1] + [0] * (n - 1) + [1])
+# Keygen
+pk, sk = keygen(n, q, poly_mod)
+# Encryption
+import sys
+pt1 = int(sys.argv[1])
+pt2 = int(sys.argv[2])
+cst1 = int(sys.argv[3])
+cst2 = int(sys.argv[4])
+# pt1 = int(input('pt1: '))
+# pt2 = int(input('pt2: '))
+# cst1 = int(input('cst1: '))
+# cst2 = int(input('cst2: '))
+start_time = time.time()
+ct1 = encrypt(pk, n, q, t, poly_mod, pt1)
+ct2 = encrypt(pk, n, q, t, poly_mod, pt2)
+
+print("[+] Ciphertext ct1({}):".format(pt1))
+print("")
+print("\t ct1_0:", ct1[0])
+print("\t ct1_1:", ct1[1])
+print("")
+print("<p class='ct2'>[+] Ciphertext ct2({}):".format(pt2))
+print("")
+print("\t ct1_0:", ct2[0])
+print("\t ct1_1:", ct2[1])
+print("</p>")
+
+# Evaluation
+ct3 = add_plain(ct1, cst1, q, t, poly_mod)
+ct4 = mul_plain(ct2, cst2, q, t, poly_mod)
+
+# Decryption
+decrypted_ct3 = decrypt(sk, n, q, t, poly_mod, ct3)
+decrypted_ct4 = decrypt(sk, n, q, t, poly_mod, ct4)
+end_time = time.time()
+print("<p class='dc1'>[+] Decrypted ct3(ct1 + {}): {}</p>".format(cst1, decrypted_ct3))
+print("<p class='dc2'>[+] Decrypted ct4(ct2 * {}): {}</p>".format(cst2, decrypted_ct4))
+
+print(f"<p class='otp'>\n\n\n\n\n\nExecution time: {end_time-start_time} seconds</p>")
